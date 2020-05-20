a536fb086abacc214bac6c0f4f1a6e017c5b95f8
Yousef Nourizadeh <nou.yousef@gmail.com>
2020-05-20 12:39:47 +0400

    weather app finalized

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const request = require('request')
-
-const url = 'http://api.weatherstack.com/current?access_key=2eea00c0ec9c1a6191ade9fe3c7a5734&query=23.568728,58.460181&units=m'
-
-request({ url: url, json: true }, (error, response) => {
-    // console.log(response.body.current);
-    console.log(response.body.current.weather_descriptions[0] + '. It is currently', response.body.current.temperature, 'degrees. humidity is', response.body.current.humidity);
-})
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+const chalk = require('chalk');
+// const yargs = require('yargs').argv[2];
+const address = process.argv[2];
+if (address) {
+    geocode(address, (error, { longitude, latitude, location } = {}) => {
+        if (error) {
+            return console.log(error)
+        }
+        forecast(longitude, latitude, (error, { description, temperature, humidity } = {}) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log(chalk.green(location));
+            console.log('Situation:', description);
+            console.log('Temperature:', temperature);
+            console.log('Humidity:', humidity);
+        })
+    })
+} else {
+    console.log('Please provide a city name')
+}
